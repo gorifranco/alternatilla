68b2370b7c90b4bc195e4da8179cc06480e11870
gorifranco <102065110+gorifranco@users.noreply.github.com>
2025-06-05 12:18:04 +0200

    event view module model finished

--- modules/event_view/__manifest__.py
+++ modules/event_view/__manifest__.py
@@ -23,8 +23,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/event_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- modules/event_view/models/models.py
+++ modules/event_view/models/models.py
@@ -7,17 +7,48 @@ class EventViewExtension(models.Model):
     _inherit = 'event.event'
     
     cover_image = fields.Image(string="Image", max_width=1920, max_height=1920)
-    cover_image_1024 = fields.Image("Image 1024px", related="image", max_width=1024, max_height=1024, store=True)
-    cover_image_256 = fields.Image("Image 256px", related="image", max_width=256, max_height=256, store=True)
+    cover_image_1024 = fields.Image("Image 1024px", related="cover_image", max_width=1024, max_height=1024, store=True)
+    cover_image_256 = fields.Image("Image 256px", related="cover_image", max_width=256, max_height=256, store=True)
     
     imatges_web = fields.Many2many('m2m_images', string="Imatges web")
-    videos = fields.Many2many('website.video', string="Videos")
+    videos = fields.One2many('website.video', 'event_id', string="Videos")
     text_principal = fields.Html(string="Text Principal")
-    imatges_laterals = fields.Many2many('image', string="Imatges laterals")
-    cartells = fields.Many2many('image', string="Cartells")
-    compromesos = fields.Many2many('res.partner', string="Compromesos")
-    patrocinadors = fields.Many2many('res.partner', string="Patrocinadors")
-    colaboradors = fields.Many2many('res.partner', string="Colaboradors")
+    imatges_laterals = fields.Many2many(
+        'm2m_images', 
+        'event_image_laterals_rel',
+        'event_id', 'image_id', 
+        string="Imatges laterals"
+    )
+    cartells = fields.Many2many(
+        'm2m_images', 
+        'event_image_cartells_rel',
+        'event_id', 'image_id', 
+        string="Cartells"
+    )
+
+    compromesos = fields.Many2many(
+    'res.partner',
+    'event_event_res_partner_compromesos_rel',
+    'event_id',
+    'partner_id',
+    string="Compromesos"
+    )
+
+    patrocinadors = fields.Many2many(
+        'res.partner',
+        'event_event_res_partner_patrocinadors_rel',
+        'event_id',
+        'partner_id',
+        string="Patrocinadors"
+    )
+
+    colaboradors = fields.Many2many(
+        'res.partner',
+        'event_event_res_partner_colaboradors_rel',
+        'event_id',
+        'partner_id',
+        string="Colaboradors"
+    )
     
     
 class EventVideos(models.Model):
@@ -25,3 +56,5 @@ class EventVideos(models.Model):
     _description = "Videos"
     
     url = fields.Char(string="Video", required=True)
+    event_id = fields.Many2one('event.event', string="Event")
+    
